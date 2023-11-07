d7b725dcaca370e2f878f7ef16bb88ce149994b8
williamsmd11 <150103403+williamsmd11@users.noreply.github.com>
2023-11-07 00:00:43 -0500

    Create UnitTestTwo.py
    Second unit  test

---
+++ UnitTestTwo.py
@@ -0,0 +1,7 @@
+import SimpleProgram
+import unittest
+class TestSum(unittest.TestCase):
+ def test_integer(self):
+    self.assertNotEqual(SimpleProgram.divide(8,2), 5):
+if __name__ == '__main__':
+            unittest.maiin()
